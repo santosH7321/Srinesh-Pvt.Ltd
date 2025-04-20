934f79bae84506b59f54e68bf158b34fe34c1053
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-04-20 21:32:44 +0530

    login route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import User from "../models/User.js";
+import cookie from "cookie-parser";
 
-const router = express.Router();
+const authRouter = express.Router();
 
-router.post("/api/v2/signup", async (req, res) => {
+authRouter.post("/api/v2/signup", async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const hashPassword = await bcrypt.hash(password, 10);
@@ -29,3 +30,36 @@ router.post("/api/v2/signup", async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 });
+
+authRouter.post("/api/v2/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+        const token = await user.getJWT();
+        res.cookie("token", token, { expires: new Date(Date.now() + 8 * 3600000) });
+        res.status(200).json({
+            success: true,
+            message: "Login successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            },
+        });
+    } catch (error) {
+        console.error("Error during login:", error);
+        return res.status(500).json({ message: "Internal server error" });
+        
+    }
+})
+
+export default authRouter;
